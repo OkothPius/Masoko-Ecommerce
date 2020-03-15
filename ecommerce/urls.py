24ef3b9527309554f25b2971e410279a20753aae
okothpius <orukopius@gmail.com>
2020-03-15 12:28:46 +0300

    add routes of slug

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,13 +1,23 @@
 from django.conf.urls import url
-from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('product/<int:product_id>/<slug:product_slug>/',views.show_product, name='product_detail'),
-    path('cart/', views.show_cart, name='show_cart'),
-    path('checkout/', views.checkout, name='checkout'),
-    path('process-payment/', views.process_payment, name='process_payment'),
-    path('payment-done/', views.payment_done, name='payment_done'),
-    path('payment-cancelled/', views.payment_cancelled, name='payment_cancelled'),
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<product_id>\d+)/(?P<product_slug>[-\w]+)/$',views.show_product, name='product_detail'),
+    url(r'^cart/', views.show_cart, name='show_cart'),
+    url(r'^checkout/', views.checkout, name='checkout'),
+    url(r'^process-payment/', views.process_payment, name='process_payment'),
+    url(r'^payment-done/', views.payment_done, name='payment_done'),
+    url(r'^payment-cancelled/', views.payment_cancelled, name='payment_cancelled'),
 ]
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+
+
+
+
+
+    # url(r'(product/<int:product_id>/<slug:product_slug>\d+)/$',views.show_product, name='product_detail'),
